d9f5eb0843e51797d4231aec04f76c4d82c9c0da
Andrew <an.lee@Andrews-MacBook-Air.local>
2026-09-18 23:33:58 +1000

    fibonnaci

---
+++ recursion/fibo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int fibon(int n){
+ 
+  if (n <= 1){
+    return n;
+  }
+
+
+  return fibon(n - 1)  + fibon(n - 2);
+}
+
+
+int main(){
+
+
+  cout << fibon(5) << " ";
+
+}
